9272764e3e4d6402ad3d2c4eebdce3827aff9caa
wangju <wangju@shein.com>
2018-11-22 17:52:16 +0800

    add sms tool

---
+++ app/Core/Utils/Sms.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Core\Utils;
+
+
+class Sms
+{
+
+    protected static $message;
+
+    /**
+     * @param $phone
+     * @param $code
+     * @return bool
+     * @author wangju 18-11-22 下午5:51
+     */
+    public static function sendRegisterMessage($phone, $code)
+    {
+        $success = true;
+        try {
+            static::$message= easy_sms()->send($phone, [
+                'template' => 'SMS_63885240',
+                'data' => [
+                    'code' => $code
+                ],
+            ]);
+        } catch (\Exception $e) {
+            $success = false;
+            static::$message = $e->getMessage();
+        }
+        if (is_array(static::$message)) {
+            static::$message = json_encode(static::$message);
+        }
+        return $success;
+    }
+
+    public static function getMessage(){
+        return static::$message;
+    }
+
+}
